13b158b74e4cbc3de28b1d4ff5684dbe56b92867
Simone Sestito <simone@simonesestito.com>
2025-05-14 17:29:26 +0200

    Fix tensors shape

--- src/gcg/faster_gcg.py
+++ src/gcg/faster_gcg.py
@@ -5,7 +5,8 @@ import transformers
 from .context import GCGRunContext
 from .early_stop import LossEarlyStop
 from .tensor_set import TensorSet
-from .loss import LossFunction, loss_cw
+from .loss import loss_ce
+
 
 class FasterGCG:
     """
@@ -19,7 +20,7 @@ class FasterGCG:
                  adversarial_tokens_length: int,
                  top_k_substitutions_length: int,
                  vocab_size: int,
-                 loss_fn: LossFunction = loss_cw,
+                 lambda_reg_embeddings_distance: float = 4.0,
                  ):
         """
         Initialize the FasterGCG class, with the default hyperparameters for the GCG algorithm.
@@ -29,22 +30,23 @@ class FasterGCG:
         :param adversarial_tokens_length: Length of the adversarial tokens to be generated and optimized.
         :param top_k_substitutions_length: Length of the top-k substitutions to consider in the optimization process.
         :param vocab_size: Size of the vocabulary, used to generate one-hot tokens.
-        :param loss_fn: Loss function to be used for the optimization process.
+        :param lambda_reg_embeddings_distance: Weight of the regularization term, which is the distance between token embeddings.
+                                               The suggested value is 4.0, according to the paper.
         """
         self.num_iterations = num_iterations
         self.batch_size = batch_size
-        self.loss_fn = loss_fn
         self.adversarial_tokens_length = adversarial_tokens_length
         self.top_k_substitutions_length = top_k_substitutions_length
         self.vocab_size = vocab_size
+        self.lambda_reg_embeddings_distance = lambda_reg_embeddings_distance
 
 
     def tokenize_and_attack(self,
                             tokenizer: transformers.PreTrainedTokenizerBase,
                             model: transformers.PreTrainedModel,
-                            x_fixed_input: str,
+                            x_fixed_input: str | None,
                             y_target_output: str,
-                            ) -> tuple[str, int]:
+                            ) -> tuple[str, str, int]:
         """
         Tokenize the input and target output, and perform the attack on the model using the GCG algorithm.
         This function is the main entry point for the attack, when the input and target output are not already tokenized.
@@ -59,9 +61,14 @@ class FasterGCG:
         :param y_target_output:
             The target string, which is what the model should output, ideally.
         :return:
-            The adversarial suffix string, to be appended to the input string to perform the attack.
+            - The adversarial suffix string, to be appended to the input string to perform the attack.
+            - The response of the model being attacked
+            - The number of iterations performed for the optimization process.
         """
-        x_fixed_input_ids = tokenizer(x_fixed_input, return_tensors="pt").input_ids.to(model.device)
+        x_fixed_input_ids: torch.Tensor | None = None
+        if x_fixed_input is not None:
+            x_fixed_input_ids = tokenizer(x_fixed_input, return_tensors="pt").input_ids.to(model.device)
+
         y_target_output_ids = tokenizer(y_target_output, return_tensors="pt").input_ids.to(model.device)
 
         x_suffix_ids, steps = self.attack(
@@ -70,7 +77,21 @@ class FasterGCG:
             y_target_output=y_target_output_ids,
         )
         x_suffix = tokenizer.decode(x_suffix_ids.view(-1), skip_special_tokens=True)
-        return x_suffix, steps
+
+        # Get the actual response of the model being attacked
+        full_text = x_suffix_ids
+        if x_fixed_input_ids is not None:
+            full_text = torch.cat([x_fixed_input_ids, x_suffix_ids], dim=1)
+
+        with torch.no_grad():
+            y_attack_response_ids = model.generate(
+                input_ids=full_text,
+                max_new_tokens=y_target_output_ids.size(-1),
+                do_sample=False,
+            )
+            y_attack_response = tokenizer.decode(y_attack_response_ids[0], skip_special_tokens=True)
+
+        return x_suffix, y_attack_response, steps
 
 
     def attack(self,
@@ -99,20 +120,20 @@ class FasterGCG:
         model.eval()
 
         # Generate the attack input suffix to give to the model
-        x_attack_tokens = self._generate_one_hot_tokens(sequence_length=self.vocab_size)
+        x_attack_token_ids = torch.randint(0, self.vocab_size, (self.adversarial_tokens_length,), device=model.device)
 
         # Create an empty historical record set, to avoid self loop
         record_set = TensorSet(hidden_size=self.adversarial_tokens_length,
-                               device=x_attack_tokens.device,
-                               dtype=x_attack_tokens.dtype)
+                               device=x_attack_token_ids.device,
+                               dtype=x_attack_token_ids.dtype)
 
         run_context = GCGRunContext(
             model=model,
             x_fixed_input_ids=x_fixed_input,
-            x_fixed_input_embed=model.get_input_embeddings()(x_fixed_input),
+            x_fixed_input_embed=model.get_input_embeddings()(x_fixed_input) if x_fixed_input is not None else None,
             y_target_output_ids=y_target_output,
             y_target_output_embed=model.get_input_embeddings()(y_target_output),
-            x_attack_token_ids=x_attack_tokens,
+            x_attack_token_ids=x_attack_token_ids,
             record_set=record_set,
         )
 
@@ -136,20 +157,6 @@ class FasterGCG:
         return run_context.x_attack_token_ids, step_i + 1
 
 
-    @torch.no_grad()
-    def _generate_one_hot_tokens(self, sequence_length: int, device: torch.device) -> torch.Tensor:
-        """
-        Generate one-hot tokens for a random sample, of the given sequence length.
-        The one-hot sample is not batched.
-
-        :param sequence_length: Length of the sequence to be generated.
-        :param device: The device to generate the one-hot sample on.
-        :return: The one-hot sample, of shape (sequence_length, vocab_size).
-        """
-        input_ids = torch.randint(0, self.vocab_size, (sequence_length,), device=device)
-        return F.one_hot(input_ids, num_classes=self.vocab_size)
-
-
     def _compute_top_k_substitutions(self, run_context: GCGRunContext) -> torch.Tensor:
         """
         First step of GCG: compute the top-k substitutions for each token in the attack input IDs.
@@ -162,14 +169,15 @@ class FasterGCG:
             The top-k substitutions for each token in the attack input IDs,
             which is a tensor of shape (top_k_substitutions_length, vocab_size).
         """
+        print(f'{run_context.x_attack_token_ids.shape=}')
         attack_one_hot = F.one_hot(run_context.x_attack_token_ids, num_classes=self.vocab_size).float().detach()
         attack_one_hot.requires_grad_(True)
-        assert attack_one_hot.ndim == 2 or attack_one_hot.size(0) == 1, \
-            f'Expected attack_one_hot to be of shape (1, {self.adversarial_tokens_length}, {self.vocab_size}), but got {attack_one_hot.shape}'
+        assert attack_one_hot.ndim == 2, \
+            f'Expected attack_one_hot to be of shape ({self.adversarial_tokens_length}, {self.vocab_size}), but got {attack_one_hot.shape}'
 
-        loss = self._compute_gcg_loss(attack_one_hot, run_context)[0]
+        loss = self._compute_gcg_loss(attack_one_hot.unsqueeze(0), run_context)[0]
 
-        attack_one_hot_grads = torch.autograd.grad(loss, attack_one_hot)[0][0]
+        attack_one_hot_grads = torch.autograd.grad(loss, attack_one_hot)[0]     # [adversarial_tokens_length, vocab_size]
         attack_one_hot_grads /= attack_one_hot_grads.norm(dim=-1, keepdim=True)
 
         # Now, for each token in the prefix, we need to find the top-k replacements with the lowest gradient
@@ -192,17 +200,19 @@ class FasterGCG:
             The loss value for the GCG algorithm, which is a scalar tensor.
         """
         assert attack_one_hot.ndim == 3, \
-            f'Expected attack_one_hot to be batched'
+            f'Expected attack_one_hot to be batched ({attack_one_hot.ndim}D instead of 3D)'
         batch_size = attack_one_hot.size(0)
         assert attack_one_hot.shape == (batch_size, self.adversarial_tokens_length, self.vocab_size), \
             f'Expected attack_one_hot to be of shape (batch_size, {self.adversarial_tokens_length}, {self.vocab_size}), but got {attack_one_hot.shape}'
 
-        attack_embed = F.linear(attack_one_hot, run_context.model.get_input_embeddings().weight)
-        full_input_embed = torch.cat([
-            run_context.x_fixed_input_embed,
-            attack_embed,
-            run_context.y_target_output_embed
-        ], dim=1)
+        attack_embed = F.linear(attack_one_hot, run_context.model.get_input_embeddings().weight.t())
+
+        full_input_embed = []
+        if run_context.x_fixed_input_embed is not None:
+            full_input_embed.append(run_context.x_fixed_input_embed)
+        full_input_embed.append(attack_embed)
+        full_input_embed.append(run_context.y_target_output_embed)
+        full_input_embed = torch.cat(full_input_embed, dim=1)
 
         # Compute the CE-loss, using teacher forcing.
         # The use of teacher forcing SHOULD be okay, since it is used in nanoGCG:
@@ -211,7 +221,7 @@ class FasterGCG:
         output_logits = run_context.model(inputs_embeds=full_input_embed)\
                             .logits[:, -run_context.y_target_output_ids.size(-1)-1:-1]
 
-        per_sample_loss = self.loss_fn(output_logits, run_context.y_target_output_ids)
+        per_sample_loss = loss_ce(output_logits, run_context.y_target_output_ids)
         assert per_sample_loss.shape == (batch_size,), \
             f'Expected per_sample_loss to be of shape (batch_size,), but got {per_sample_loss.shape}'
 
