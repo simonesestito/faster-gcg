69b53dd7f2695b3d8816938ae77f021b589d3dbb
Simone Sestito <simone@simonesestito.com>
2025-05-19 10:13:57 +0200

    Add copyright notice

--- src/gcg/__init__.py
+++ src/gcg/__init__.py
@@ -1,1 +1,18 @@
+#  Copyright (c) 2025 Simone Sestito
+#
+#  This file is part of Faster-GGC-Lib.
+#
+#  Faster-GGC-Lib is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Faster-GGC-Lib is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Faster-GGC-Lib.  If not, see <http://www.gnu.org/licenses/>.
+
 from .faster_gcg import FasterGCG
